facba38a86da0426b934e7d3b2250eff6072965e
Robin C. Ladiges <rcl.git@blackpinguin.de>
2014-09-23 20:55:48 +0000

    Optimierungen, Kommentare

--- Code/scripts/graphics.js
+++ Code/scripts/graphics.js
@@ -1,26 +1,32 @@
 var colorList = {};
-var modelList = {};
-var geometryList = {};
+var modelList = {}; //TODO: wird hier nicht verwendet?
+var geometryList = {}; //TODO: wird hier nicht verwendet?
 var materials =[];
 //var material = new THREE.MeshFaceMaterial(materials);
 
+//Gibt den Index des 
 function getMaterialIndex(color){
-	if(colorList[color] == null){
+	//Wenn Material für die gewünschte Farbe noch nicht existiert
+	if(colorList[color] === undefined){
+		//Erstelle ein neues Material
 		var uniforms = {myColor: { type: "c", value: new THREE.Color( convertHex(color) )}};
 		var itemMaterial = new THREE.ShaderMaterial({
 			uniforms: uniforms,
+			//TODO: mögliche Optimierung. Wenn sich der Inhalt von den DOM-Elementen nie ändert,
+			//lieber in JavaScript cachen, statt jedes mal neu zu laden.
 			vertexShader: document.getElementById('2d-vertex-shader').innerHTML,
 			fragmentShader: document.getElementById('2d-fragment-shader').innerHTML
 		});
+		//speichere dieses für später
 		materials.push(itemMaterial);
+		//und merke den Index in einem assoziativen Array
 		colorList[color] = materials.length -1;
-		return materials.length -1;
-	}else{
-		return colorList[color];
 	}
+	//Index der Farbe zurückgeben
+	return colorList[color];
 }
 
+//parst einen hexadezimalen String für Farben der Form "#rrggbb" zu einem Integer
 function convertHex(hex){
-    hex = hex.replace('#','0x');
-    return parseInt(hex, 16);
+	return parseInt(hex.substr(1), 16);
 }
