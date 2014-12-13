14dedc70959de95f649c7100601807d436081be0
Robin C. Ladiges <rcl.git@blackpinguin.de>
2014-12-13 13:20:08 +0000

    Rotation der Geschütze in der Höhe.

--- DeathstarPD/Assets/Scripts/Entity/Towers/Aiming.cs
+++ DeathstarPD/Assets/Scripts/Entity/Towers/Aiming.cs
@@ -11,29 +11,43 @@ public class Aiming : MonoBehaviour {
 	private Quaternion startRot;
 	private Vector3 barrelAnchor;
 
+	private Tower self;
+
 	// Use this for initialization
 	void Start () {
+		self = gameObject.GetComponent<Tower>();
 		startRot = basis.rotation;
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		target = gameObject.GetComponent<Tower>().Target;
-		if(target){
+		target = self.Target;
+
+		if(target != null && !target.IsDead && target.enabled){
+
+			//Rotation der Basis
 			basis.rotation = startRot;
 			Vector3 delta = basis.InverseTransformPoint(target.Pos);
 			float height = delta.y;
 			delta.y = 0;
 			delta = basis.TransformPoint(delta);
-
 			Debug.DrawLine(basis.position, delta, Color.blue);
-
 			basis.LookAt(delta, basis.TransformPoint(Vector3.up));
 			basis.Rotate(0,-90,0);
-			//Barrel Ausrichtung
-			//float dist = Vector3.Distance(barrel.position, target.Pos);
-			//float angle = Mathf.Asin(height/dist);
-			//barrel.Rotate(0, 0, angle);
+
+			// Rotation des Geschützes
+			// up vector
+			Vector3 up = self.Pos;
+			// vector to target
+			Vector3 toTarget = target.Pos - self.Pos;
+			//angle between up and target
+			float angle = (90f - Vector3.Angle(up, toTarget));
+			Utility.MinMax(ref angle, 0f, 90f);
+			Vector3 r = barrel.localEulerAngles;
+			r.z = angle;
+			barrel.localEulerAngles = r;
 		}
 	}
 }
