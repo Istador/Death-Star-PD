b380d40d235c2a3568414c4f9fd389a631901189
Robin C. Ladiges <rcl.git@blackpinguin.de>
2015-11-02 19:07:42 +0000

    DCSP Einreichung, Skizze aufsplitten

---
+++ Dokumentation/DCSP/Einreichung/Deathstar PD _ Deutscher Computerspielpreis-Dateien/js_R7tjQ1PeXsKuos_oPApG-MImjbuWZO9q4WM-YjnEWb0.js
@@ -0,0 +1,146 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n!=1)); }, 'strings': {"":{"An AJAX HTTP error occurred.":"Ein AJAX-HTTP-Fehler ist aufgetreten.","HTTP Result Code: !status":"HTTP-R\u00fcckgabe-Code: !status","An AJAX HTTP request terminated abnormally.":"Eine AJAX-Anfrage ist abnormal beendet worden.","Debugging information follows.":"Im Folgenden finden Sie Debugging-Informationen.","Path: !uri":"Pfad: !uri","StatusText: !statusText":"Statustext: !statusText","ResponseText: !responseText":"Antworttext: !responseText","ReadyState: !readyState":"ReadyState: !readyState","Next":"Weiter","Cancel":"Abbrechen","Disabled":"Deaktiviert","Enabled":"Aktiviert","Edit":"Bearbeiten","none":"keine","Sunday":"Sonntag","Monday":"Montag","Tuesday":"Dienstag","Wednesday":"Mittwoch","Thursday":"Donnerstag","Friday":"Freitag","Saturday":"Samstag","Add":"Hinzuf\u00fcgen","Upload":"Hochladen","Done":"Fertig","OK":"OK","Prev":"Vorheriges","Mon":"Mo","Tue":"Di","Wed":"Mi","Thu":"Do","Fri":"Fr","Sat":"Sa","Sun":"So","January":"Januar","February":"Februar","March":"M\u00e4rz","April":"April","May":"Mai","June":"Juni","July":"Juli","August":"August","September":"September","October":"Oktober","November":"November","December":"Dezember","Show":"Anzeigen","Select all rows in this table":"Alle Zeilen dieser Tabelle ausw\u00e4hlen","Deselect all rows in this table":"Alle Zeilen dieser Tabelle abw\u00e4hlen","Today":"Heute","Jan":"Jan","Feb":"Feb","Mar":"M\u00e4r","Apr":"Apr","Jun":"Jun","Jul":"Jul","Aug":"Aug","Sep":"Sep","Oct":"Okt","Nov":"Nov","Dec":"Dez","Su":"So","Mo":"Mo","Tu":"Di","We":"Mi","Th":"Do","Fr":"Fr","Sa":"Sa","Not published":"Nicht ver\u00f6ffentlicht","Please wait...":"Bitte warten...","Hide":"Ausblenden","mm\/dd\/yy":"mm\/tt\/jj","Only files with the following extensions are allowed: %files-allowed.":"Nur Dateien mit folgenden Erweiterungen sind zul\u00e4ssig: %files-allowed.","By @name on @date":"Von @name am @date","By @name":"Von @name","Not in menu":"Nicht im Men\u00fc","Alias: @alias":"Alias: @alias","No alias":"Kein Alias","New revision":"Neue Version","Drag to re-order":"Ziehen, um die Reihenfolge zu \u00e4ndern","Changes made in this table will not be saved until the form is submitted.":"\u00c4nderungen in dieser Tabelle werden nicht gespeichert, bis dieses Formular abgesendet wurde.","The changes to these blocks will not be saved until the \u003Cem\u003ESave blocks\u003C\/em\u003E button is clicked.":"Die \u00c4nderungen an diesen Bl\u00f6cken werden nicht gespeichert, bis auf dem \u003Cem\u003EBl\u00f6cke speichern\u003C\/em\u003E-Button geklickt wurde.","This permission is inherited from the authenticated user role.":"Diese Berechtigung wird von der Rolle \u201aAuthentifizierte Benutzer\u2018 ererbt.","No revision":"Keine Version","Requires a title":"Ben\u00f6tigt einen Titel","Not restricted":"Uneingeschr\u00e4nkt","(active tab)":"(aktiver Reiter)","Not customizable":"Nicht anpassbar","Restricted to certain pages":"Auf bestimmte Seiten eingeschr\u00e4nkt","The block cannot be placed in this region.":"Der Block kann nicht in dieser Region abgelegt werden.","Hide summary":"Zusammenfassung verbergen","Edit summary":"Zusammenfassung bearbeiten","Don\u0027t display post information":"Beitragsinformationen nicht anzeigen","The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.":"Die ausgew\u00e4hlte Datei %filename konnte nicht hochgeladen werden. Nur Dateien mit den folgenden Erweiterungen sind zul\u00e4ssig: %extensions.","Re-order rows by numerical weight instead of dragging.":"Zeilen mittels numerischer Gewichtung ordnen statt mit Drag-and-Drop","Show row weights":"Zeilenreihenfolge anzeigen","Hide row weights":"Zeilenreihenfolge ausblenden","Autocomplete popup":"Popup zur automatischen Vervollst\u00e4ndigung","Searching for matches...":"Suche \u2026","Other":"Andere","Remove group":"Gruppe entfernen","Apply (all displays)":"Anwenden (alle Anzeigen)","Revert to default":"Auf Standardwert zur\u00fccksetzen","Apply (this display)":"Anwenden (diese Anzeige)","You can not perform this operation.":"Sie k\u00f6nnen diesen Vorgang nicht durchf\u00fchren.","Do you want to refresh the current directory?":"Soll das aktuelle Verzeichnis aktualisiert werden?","Delete selected files?":"Ausgew\u00e4hlte Dateien l\u00f6schen?","Please select a thumbnail.":"Bitte w\u00e4hlen Sie ein Miniaturbild aus.","Please specify dimensions within the allowed range that is from 1x1 to @dimensions.":"Die Abmessungen der Bilder sind nur in dem Bereich von 1x1 bis @dimensions zul\u00e4ssig.","Please select a file.":"Bitte eine Datei w\u00e4hlen.","Log messages":"Logmitteilungen","%filename is not an image.":"Die Datei %filename ist kein Bild.","You must select at least %num files.":"Sie m\u00fcssen mindestens %num Dateien ausw\u00e4hlen.","You are not allowed to operate on more than %num files.":"Sie d\u00fcrfen nicht mehr als %num Dateien gleichzeitig bearbeiten.","Close":"Schlie\u00dfen","Insert file":"Datei einf\u00fcgen","all":"Alle","Change view":"Ansicht \u00e4ndern","Resume":"Weiter","Pause":"Pause","Submit":"Absenden","Loading...":"Wird geladen ...","Cannot continue, nothing selected":"Zum Fortsetzen w\u00e4hlen Sie etwas aus","Internal server error. Please see server or PHP logs for error information.":"Interner Serverfehler. Pr\u00fcfen Sie die Server- bzw. PHP-Protokolle.","Available tokens":"Verf\u00fcgbare Token","Insert this token into your form":"Dieses Token in Ihr Formular einf\u00fcgen","First click a text field to insert your tokens into.":"Klicken Sie zun\u00e4chst auf ein Textfeld, um Ihr Token einzuf\u00fcgen.","Automatic alias":"Automatischer Alias","Scheduled for publishing":"Geplant zur Ver\u00f6ffentlichung","Scheduled for unpublishing":"Geplant zur Deaktivierung","Not scheduled":"nicht geplant","Publishing enabled":"Ver\u00f6ffentliichen aktiviert","Unpublishing enabled":"Deaktivierung aktiviert","Show description":"Beschreibung anzeigen","Select":"Auswahl","Add file":"Datei hinzuf\u00fcgen","One domain with multiple subdomains":"Eine Domain mit mehreren Sub-Domains","Multiple top-level domains":"Mehrere Top-Level-Domains","Universal web tracking opt-out":"Universal web tracking opt-out","All pages with exceptions":"Alle Seiten mit Ausnahmen","Excepted: @roles":"Ausgenommen: @roles","On by default with opt out":"Standardm\u00e4\u00dfig aktiv, mit Opt-out","Off by default with opt in":"Standardm\u00e4\u00dfig deaktiviert, mit Opt-out","Outbound links":"Ausgehende Links","Mailto links":"Mailto-Links","Downloads":"Downloads","Not tracked":"Nicht getrackt","@items enabled":"@items aktiviert","Site search":"Website-Suche","A single domain":"Eine einzelne Domain","No privacy":"Keine Privatsph\u00e4re"}} };;
+(function ($) {  
+  
+  Drupal.behaviors.globalJS = {
+    attach: function (context, settings) {
+        
+//        $("#sidr-0-button").attr('title', 'Menü');
+//        $("#sidr-0-button").html('WAAAAAAAAAAAAs');
+//        alert('YO'+$("#sidr-0-button").attr('href'));
+        
+    }
+  };
+
+  Drupal.dcsp = Drupal.dcsp || {}
+  Drupal.dcsp.getThrobber = function(element, size) {
+    $(element).html('<div class="throbber ' + size + '"></div>');
+  };
+  
+})(jQuery);
+;
+(function($){Drupal.behaviors.image_caption={attach:function(context,settings){$("img.caption:not(.caption-processed)").each(function(i){var imgwidth=$(this).width()?$(this).width():false;var imgheight=$(this).height()?$(this).height():false;var captiontext=$(this).attr('title');if($(this).attr('align')){var alignment=$(this).attr('align');$(this).css({'float':alignment});$(this).removeAttr('align');}else if($(this).css('float')){var alignment=$(this).css('float');}else{var alignment='normal';}
+var style=$(this).attr('style')?$(this).attr('style'):'';$(this).removeAttr('width');$(this).removeAttr('height');$(this).css('width','');$(this).css('height','');$(this).removeAttr('align');$(this).removeAttr('style');$(this).wrap("<span class=\"image-caption-container\" style=\"display:inline-block;"+style+"\"></span>");$(this).parent().addClass('image-caption-container-'+alignment);if(imgwidth){$(this).width(imgwidth);$(this).parent().width(imgwidth);}
+if(imgheight){$(this).height(imgheight);}
+$(this).parent().append("<span style=\"display:block;\" class=\"image-caption\">"+captiontext+"</span>");$(this).addClass('caption-processed');});}};})(jQuery);;
+/**
+ * @file
+ * Integrate Sidr library with Responsive Menus.
+ */
+(function ($) {
+  /**
+   * Preparation for each element Sidr will affect.
+   */
+  function sidr_it(menuElement, ind, iteration, $windowWidth) {
+    // Only apply if window size is correct.
+    var $media_size = iteration.media_size || 768;
+    // Call Sidr with our settings.
+    $(menuElement).once('responsive-menus-sidr', function() {
+      var $id = 'sidr-' + ind;
+      var $wrapper_id = 'sidr-wrapper-' + ind;
+      $(this).before('<div id="' + $wrapper_id + '"><a id="' + $id + '-button" title="Menü" href="#' + $id + '">' + iteration.trigger_txt + '</a></div>');
+      $('#' + $wrapper_id).hide();
+      if ($windowWidth <= $media_size) {
+        $('#' + $wrapper_id).show();
+        $(this).hide();
+      }
+      // Set 1/0 to true/false respectively.
+      $.each(iteration, function(key, value) {
+        if (value == 0) {
+          iteration[key] = false;
+        }
+        if (value == 1) {
+          iteration[key] = true;
+        }
+      });
+      // Sidr power go.
+      $('#' + $id + '-button').sidr({
+        name: $id || "sidr",
+        speed: parseInt(iteration.speed) || 200,
+        side: iteration.side || "left",
+        source: iteration.selectors[ind] || "#main-menu",
+        displace: iteration.displace,
+        renaming: iteration.renaming,
+        onOpen: function() { eval(iteration.onOpen); } || function() {},
+        onClose: function() { eval(iteration.onClose); } || function() {}
+      });
+    });
+  }
+
+
+  /**
+   * Main loop.
+   */
+  Drupal.behaviors.responsive_menus_sidr = {
+    attach: function (context, settings) {
+      settings.responsive_menus = settings.responsive_menus || {};
+      var $windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      $.each(settings.responsive_menus, function(ind, iteration) {
+        if (iteration.responsive_menus_style != 'sidr') {
+          return true;
+        }
+        if (!iteration.selectors.length) {
+          return;
+        }
+        // Iterate each selector.
+        $.each(iteration.selectors, function(index, value) {
+          // Stop if there is no menu element.
+          if ($(value).length < 1) {
+            return true;
+          }
+          // Multi-level (selector hits multiple ul's).
+          if ($(value).length > 1) {
+              $(value).each(function(val_index) {
+                if (!$(this).parents('ul').length) {
+                  sidr_it(this, index, iteration, $windowWidth);
+                }
+              });
+            }
+            else {
+              // Single level.
+              sidr_it(value, index, iteration, $windowWidth);
+            }
+        });
+      });
+
+      // Handle window resizing.
+      $(window).resize(function() {
+        // Window width with legacy browsers.
+        $windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        $.each(settings.responsive_menus, function(ind, iteration) {
+          if (iteration.responsive_menus_style != 'sidr') {
+            return true;
+          }
+          if (!iteration.selectors.length) {
+            return;
+          }
+          // Iterate each selector.
+          $.each(iteration.selectors, function(index, value) {
+            // Stop if there is no menu element.
+            if ($(value).length < 1) {
+              return true;
+            }
+            var $wrapper_id = 'sidr-wrapper-' + index;
+            $media_size = iteration.media_size || 768;
+            if ($windowWidth <= $media_size) {
+              if (!$(value).hasClass('sidr-hidden')) {
+                $('#' + $wrapper_id).show();
+                $(value).hide().addClass('sidr-hidden');
+              }
+            }
+            else {
+              if ($(value).hasClass('sidr-hidden')) {
+                $('#' + $wrapper_id).hide();
+                $(value).show().removeClass('sidr-hidden');
+              }
+            }
+          });
+        });
+      });
+    }
+  };
+}(jQuery));
+;
+/*! Sidr - v1.2.1 - 2013-11-06
+ * https://github.com/artberri/sidr
+ * Copyright (c) 2013 Alberto Varela; Licensed MIT */
+(function(e){var t=!1,i=!1,n={isUrl:function(e){var t=RegExp("^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$","i");return t.test(e)?!0:!1},loadContent:function(e,t){e.html(t)},addPrefix:function(e){var t=e.attr("id"),i=e.attr("class");"string"==typeof t&&""!==t&&e.attr("id",t.replace(/([A-Za-z0-9_.\-]+)/g,"sidr-id-$1")),"string"==typeof i&&""!==i&&"sidr-inner"!==i&&e.attr("class",i.replace(/([A-Za-z0-9_.\-]+)/g,"sidr-class-$1")),e.removeAttr("style")},execute:function(n,s,a){"function"==typeof s?(a=s,s="sidr"):s||(s="sidr");var r,d,l,c=e("#"+s),u=e(c.data("body")),f=e("html"),p=c.outerWidth(!0),g=c.data("speed"),h=c.data("side"),m=c.data("displace"),v=c.data("onOpen"),y=c.data("onClose"),x="sidr"===s?"sidr-open":"sidr-open "+s+"-open";if("open"===n||"toggle"===n&&!c.is(":visible")){if(c.is(":visible")||t)return;if(i!==!1)return o.close(i,function(){o.open(s)}),void 0;t=!0,"left"===h?(r={left:p+"px"},d={left:"0px"}):(r={right:p+"px"},d={right:"0px"}),u.is("body")&&(l=f.scrollTop(),f.css("overflow-x","hidden").scrollTop(l)),m?u.addClass("sidr-animating").css({width:u.width(),position:"absolute"}).animate(r,g,function(){e(this).addClass(x)}):setTimeout(function(){e(this).addClass(x)},g),c.css("display","block").animate(d,g,function(){t=!1,i=s,"function"==typeof a&&a(s),u.removeClass("sidr-animating")}),v()}else{if(!c.is(":visible")||t)return;t=!0,"left"===h?(r={left:0},d={left:"-"+p+"px"}):(r={right:0},d={right:"-"+p+"px"}),u.is("body")&&(l=f.scrollTop(),f.removeAttr("style").scrollTop(l)),u.addClass("sidr-animating").animate(r,g).removeClass(x),c.animate(d,g,function(){c.removeAttr("style").hide(),u.removeAttr("style"),e("html").removeAttr("style"),t=!1,i=!1,"function"==typeof a&&a(s),u.removeClass("sidr-animating")}),y()}}},o={open:function(e,t){n.execute("open",e,t)},close:function(e,t){n.execute("close",e,t)},toggle:function(e,t){n.execute("toggle",e,t)},toogle:function(e,t){n.execute("toggle",e,t)}};e.sidr=function(t){return o[t]?o[t].apply(this,Array.prototype.slice.call(arguments,1)):"function"!=typeof t&&"string"!=typeof t&&t?(e.error("Method "+t+" does not exist on jQuery.sidr"),void 0):o.toggle.apply(this,arguments)},e.fn.sidr=function(t){var i=e.extend({name:"sidr",speed:200,side:"left",source:null,renaming:!0,body:"body",displace:!0,onOpen:function(){},onClose:function(){}},t),s=i.name,a=e("#"+s);if(0===a.length&&(a=e("<div />").attr("id",s).appendTo(e("body"))),a.addClass("sidr").addClass(i.side).data({speed:i.speed,side:i.side,body:i.body,displace:i.displace,onOpen:i.onOpen,onClose:i.onClose}),"function"==typeof i.source){var r=i.source(s);n.loadContent(a,r)}else if("string"==typeof i.source&&n.isUrl(i.source))e.get(i.source,function(e){n.loadContent(a,e)});else if("string"==typeof i.source){var d="",l=i.source.split(",");if(e.each(l,function(t,i){d+='<div class="sidr-inner">'+e(i).html()+"</div>"}),i.renaming){var c=e("<div />").html(d);c.find("*").each(function(t,i){var o=e(i);n.addPrefix(o)}),d=c.html()}n.loadContent(a,d)}else null!==i.source&&e.error("Invalid Sidr Source");return this.each(function(){var t=e(this),i=t.data("sidr");i||(t.data("sidr",s),"ontouchstart"in document.documentElement?(t.bind("touchstart",function(e){e.originalEvent.touches[0],this.touched=e.timeStamp}),t.bind("touchend",function(e){var t=Math.abs(e.timeStamp-this.touched);200>t&&(e.preventDefault(),o.toggle(s))})):t.click(function(e){e.preventDefault(),o.toggle(s)}))})}})(jQuery);;
